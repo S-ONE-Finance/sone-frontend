5f85063a34d4f9d0ba444b0a9b3226bcbb58e918
Tung Pham <tungpv@vnext.com.vn>
2021-03-18 11:32:00 +0700

    clear cache git

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -4,7 +4,7 @@ import { AbstractConnector } from '@web3-react/abstract-connector'
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { fortmatic, injected, portis, walletconnect, walletlink } from '../connectors'
 
-export const ROUTER_ADDRESS = '0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D'
+export const ROUTER_ADDRESS = '0xF48162DBf2B03004dF354c6011d7f42294aDCbA6'
 
 export const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000'
 
