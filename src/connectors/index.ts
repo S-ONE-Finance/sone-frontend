5bb944c29b4cebe95c25c7ca701c0483fa23b942
dungnh <dungnh@vnext.com.vn>
2021-05-29 17:25:04 +0700

    Remove redundant variable in `.env`
    * Remove redundant comments and format code

--- src/connectors/index.ts
+++ src/connectors/index.ts
@@ -3,10 +3,11 @@ import { InjectedConnector } from '@web3-react/injected-connector'
 import { WalletConnectConnector } from '@web3-react/walletconnect-connector'
 import { WalletLinkConnector } from '@web3-react/walletlink-connector'
 import { PortisConnector } from '@web3-react/portis-connector'
-import getLibrary from '../utils/getLibrary'
 
 import { FortmaticConnector } from './Fortmatic'
 import { NetworkConnector } from './NetworkConnector'
+import getLibrary from '../utils/getLibrary'
+
 import UNISWAP_LOGO_URL from '../assets/svg/logo.svg'
 
 const INFURA_KEY = process.env.REACT_APP_INFURA_KEY
@@ -18,15 +19,6 @@ if (typeof INFURA_KEY === 'undefined') {
   throw new Error(`REACT_APP_INFURA_KEY must be a defined environment variable`)
 }
 
-// const NETWORK_URLS: {
-//   [chainId in ChainId]: string
-// } = {
-//   [ChainId.MAINNET]: `https://mainnet.infura.io/v3/${INFURA_KEY}`,
-//   [ChainId.RINKEBY]: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
-//   [ChainId.ROPSTEN]: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
-//   [ChainId.GÖRLI]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
-//   [ChainId.KOVAN]: `https://kovan.infura.io/v3/${INFURA_KEY}`,
-// }
 const NETWORK_URLS: {
   [chainId: number]: string
 } = {
@@ -34,14 +26,14 @@ const NETWORK_URLS: {
   [4]: `https://rinkeby.infura.io/v3/${INFURA_KEY}`,
   [3]: `https://ropsten.infura.io/v3/${INFURA_KEY}`,
   [5]: `https://goerli.infura.io/v3/${INFURA_KEY}`,
-  [42]: `https://kovan.infura.io/v3/${INFURA_KEY}`,
+  [42]: `https://kovan.infura.io/v3/${INFURA_KEY}`
 }
 
 const SUPPORTED_CHAIN_IDS = [1, 4, 3, 42, 5]
 
 export const network = new NetworkConnector({
   urls: NETWORK_URLS,
-  defaultChainId: 1,
+  defaultChainId: 1
 })
 
 let networkLibrary: Web3Provider | undefined
@@ -50,7 +42,7 @@ export function getNetworkLibrary(): Web3Provider {
 }
 
 export const injected = new InjectedConnector({
-  supportedChainIds: SUPPORTED_CHAIN_IDS,
+  supportedChainIds: SUPPORTED_CHAIN_IDS
 })
 
 export const walletconnect = new WalletConnectConnector({
