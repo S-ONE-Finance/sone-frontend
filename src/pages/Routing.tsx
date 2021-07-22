e410df7bd838e975f928fdb1b9b4caf7762ce080
dungnh <dungnh@vnext.com.vn>
2021-07-23 06:40:49 +0700

    temp

--- src/pages/Routing.tsx
+++ src/pages/Routing.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Route, Switch } from 'react-router-dom'
 import Swap from './Swap'
 import { OpenClaimAddressModalAndRedirectToSwap, RedirectPathToSwapOnly, RedirectToSwap } from './Swap/redirects'
@@ -12,8 +13,7 @@ import Vote from './Vote'
 import VotePage from './Vote/VotePage'
 import Earn from './Earn'
 import Manage from './Earn/Manage'
-import React from 'react'
-import Unstake from './Unstake'
+import UnstakeUI from './UnstakeUI'
 
 export default function Routing() {
   return (
@@ -27,7 +27,7 @@ export default function Routing() {
       <Route exact strict path="/my-account/withdraw/:tokens" component={RedirectOldRemoveLiquidityPathStructure} />
       <Route exact strict path="/my-account/withdraw/:currencyIdA/:currencyIdB" component={WithdrawLiquidity} />
       <Route exact strict path="/my-account/withdraw2/:currencyIdA/:currencyIdB" component={WithdrawLiquidity2} />
-      <Route exact strict path="/my-account/unstake" component={Unstake} />
+      <Route exact strict path="/my-account/unstake" component={UnstakeUI} />
       {/* Component PoolFinder để import Pool, hiện tại trong requirements của sone chưa có use-case này. */}
       <Route exact strict path="/find" component={PoolFinder} />
       {/* Nhưng route dưới đây là của uni, trong sone không có, nhưng nên giữ lại. */}
