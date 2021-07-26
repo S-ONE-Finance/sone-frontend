02d2b8399adba3104e53ca6294fd7495a846a275
dungnh <dungnh@vnext.com.vn>
2021-07-26 09:12:56 +0700

    refactor: push logic into my staking ui (but not good yet)

--- src/pages/MyAccount/index.tsx
+++ src/pages/MyAccount/index.tsx
@@ -1,26 +1,33 @@
 import React from 'react'
-import { useTranslation } from 'react-i18next'
-import MyStaking from './MyStaking'
-import { useActiveWeb3React } from '../../hooks'
 import { Redirect } from 'react-router'
+import { useTranslation } from 'react-i18next'
+
+import { useActiveWeb3React } from '../../hooks'
+import useAccountIsReferrer from '../../hooks/useAccountIsReferrer'
+import { useIsReferralWorksOnCurrentNetwork } from '../../state/referral/hooks'
+
+import MyBalance from './MyBalance'
+import MyLiquidity from './MyLiquidity'
+import MyStaking from './MyStaking'
+import Referral from './Referral'
 import { MyAccountWrapper, PageTitleMobileOnly, Sections } from './components'
 
 export default function MyAccount() {
   const { t } = useTranslation()
   const { account } = useActiveWeb3React()
 
-  // const accountIsReferrer = useAccountIsReferrer()
-  // const isReferralWorksOnCurrentNetwork = useIsReferralWorksOnCurrentNetwork()
+  const accountIsReferrer = useAccountIsReferrer()
+  const isReferralWorksOnCurrentNetwork = useIsReferralWorksOnCurrentNetwork()
 
   if (account) {
     return (
       <MyAccountWrapper>
         <PageTitleMobileOnly>{t('My Account')}</PageTitleMobileOnly>
         <Sections>
-          {/*<MyBalance />*/}
-          {/*<MyLiquidity />*/}
+          <MyBalance />
+          <MyLiquidity />
           <MyStaking />
-          {/*{isReferralWorksOnCurrentNetwork && accountIsReferrer && <Referral />}*/}
+          {isReferralWorksOnCurrentNetwork && accountIsReferrer && <Referral />}
         </Sections>
       </MyAccountWrapper>
     )
