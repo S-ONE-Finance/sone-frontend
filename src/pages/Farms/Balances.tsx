3963ddf723d3ecdb642154c71f7d257daf9cc299
thanhnv2 <thanhnv2@vnext.com.vn>
2021-09-27 16:57:08 +0700

    total supply sone

--- src/pages/Farms/Balances.tsx
+++ src/pages/Farms/Balances.tsx
@@ -6,7 +6,6 @@ import { BigNumber } from 'ethers'
 import { useTranslation } from 'react-i18next'
 import { pxToRem } from '../../utils/PxToRem'
 import { getBalanceStringCommas } from '../../utils/formatNumber'
-import { useActiveWeb3React } from '../../hooks'
 
 interface BalanceProps {
   circulatingSupplyValue: string
@@ -14,20 +13,19 @@ interface BalanceProps {
 
 const Balances: FC<BalanceProps> = ({ circulatingSupplyValue }) => {
   const { t } = useTranslation()
-  const { account } = useActiveWeb3React()
   const [totalSupply, setTotalSupply] = useState<string>()
 
   const soneContract: Contract | null = useSoneContract()
 
   useEffect(() => {
     ;(async () => {
-      if (account) {
+      if (soneContract) {
         const balanceDataRaw: BigNumber = await soneContract?.totalSupply()
         const balanceData = getBalanceStringCommas(balanceDataRaw.toString())
         setTotalSupply(balanceData)
       }
     })()
-  }, [soneContract, account])
+  }, [soneContract])
 
   return (
     <>
