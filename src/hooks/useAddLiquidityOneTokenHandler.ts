325a00adfaf028848e82f7410edab54aa2e15f8a
thanhnv2 <thanhnv2@vnext.com.vn>
2021-09-29 15:23:02 +0700

    test add one mode

--- src/hooks/useAddLiquidityOneTokenHandler.ts
+++ src/hooks/useAddLiquidityOneTokenHandler.ts
@@ -56,7 +56,7 @@ export default function useAddLiquidityOneTokenHandler({
       return
 
     const router = getRouterContract(chainId, library, account)
-    // const isSelectedToken0 = selectedTokenParsedAmount.token.equals(selectedPair.token0)
+    const isSelectedToken0 = selectedTokenParsedAmount.token.equals(selectedPair.token0)
     const [
       selectedTokenUserInputAmountJSBI,
       selectedTokenMinAmountJSBI,
@@ -85,12 +85,12 @@ export default function useAddLiquidityOneTokenHandler({
       method = router.addLiquidityOneTokenETHExactETH
       // args = [amountTokenMin, amountETHMin, amountOutTokenMin, path, to, deadline]
       args = [
-        0,
-        0,
-        0,
-        // theOtherTokenMinAmountJSBI.toString(),
-        // selectedTokenMinAmountJSBI.toString(),
-        // theOtherTokenMinOutputAmountJSBI.toString(),
+        // 0,
+        // 0,
+        // 0,
+        theOtherTokenMinAmountJSBI.toString(),
+        selectedTokenMinAmountJSBI.toString(),
+        theOtherTokenMinOutputAmountJSBI.toString(),
         [selectedTokenParsedAmount.token.address, theOtherTokenParsedAmount.token.address],
         account,
         deadline.toHexString()
@@ -105,12 +105,12 @@ export default function useAddLiquidityOneTokenHandler({
       // args = [amountIn, amountTokenMin, amountETHMin, amountOutETHMin, path, to, deadline]
       args = [
         selectedTokenUserInputAmountJSBI.toString(),
-        // selectedTokenMinAmountJSBI.toString(),
-        // theOtherTokenMinAmountJSBI.toString(),
-        // theOtherTokenMinOutputAmountJSBI.toString(),
-        0,
-        0,
-        0,
+        selectedTokenMinAmountJSBI.toString(),
+        theOtherTokenMinAmountJSBI.toString(),
+        theOtherTokenMinOutputAmountJSBI.toString(),
+        // 0,
+        // 0,
+        // 0,
         [selectedTokenParsedAmount.token.address, theOtherTokenParsedAmount.token.address],
         account,
         deadline.toHexString()
@@ -141,12 +141,12 @@ export default function useAddLiquidityOneTokenHandler({
       // args = [amountIn, amountAMin, amountBMin, amountOutMin, path, to, deadline]
       args = [
         selectedTokenUserInputAmountJSBI.toString(),
-        // isSelectedToken0 ? selectedTokenMinAmountJSBI.toString() : theOtherTokenMinAmountJSBI.toString(),
-        // isSelectedToken0 ? theOtherTokenMinAmountJSBI.toString() : selectedTokenMinAmountJSBI.toString(),
-        // theOtherTokenMinOutputAmountJSBI.toString(),
-        0,
-        0,
-        0,
+        selectedTokenMinAmountJSBI.toString(),
+        theOtherTokenMinAmountJSBI.toString(),
+        theOtherTokenMinOutputAmountJSBI.toString(),
+        // 0,
+        // 0,
+        // 0,
         [selectedTokenParsedAmount.token.address, theOtherTokenParsedAmount.token.address],
         account,
         deadline.toHexString()
